929e5e1c067fe1248f437882ef1d2f9944ef7038
MD Mehedi Hasan <mehidy.gb@gmail.com>
2023-05-31 12:14:38 +0600

    Stable Release 2.0.5

--- src/request/Request.java
+++ src/request/Request.java
@@ -1,37 +1,10 @@
 package request;
 
-import java.util.regex.*;
-
-import controller.Calculator;
-import request.cases.*;
+import exception.CalculatorExceptions;
+import request.chain.RequestChain;
 
 public class Request {
-
-    IRequest operatorCaseHandler;
-    IRequest operandCaseHandler;
-    IRequest braceCaseHandler;
-    IRequest defaultHandler;
-    public Request() {
-        this.braceCaseHandler = new braceCaseHandler();
-        this.operatorCaseHandler = new operatorCaseHandler();
-        this.operandCaseHandler = new operandCaseHandler();
-        this.defaultHandler = new defaultHandler();
-
-        this.braceCaseHandler.setNext(this.operatorCaseHandler);
-        this.operatorCaseHandler.setNext(this.operandCaseHandler);
-        this.operandCaseHandler.setNext(this.defaultHandler);
-    }
-    public static String validateInput(String input) {
-//        String regexPattern = "\\d+\\.\\d+|\\d+|[+\\-*/()]|(sin|cos|tan)";
-//        String operatorPattern = "[+\\-*/]";
-//        Pattern pattern = Pattern.compile(regexPattern);
-//        Matcher matcher = pattern.matcher(input);
-//
-//        if (matcher.matches()) {
-//            return input.replaceAll("\\s+", "");
-//        } else {
-//            Calculator.showErrorMessage(Calculator.ERROR_MESSAGE);
-            return null; // or throw an exception, depending on your requirements
-//        }
+    public static String validated(String input) throws CalculatorExceptions {
+       return new RequestChain().handle(input);
     }
 }
